f9adbfd9099319b66b39449797502e5093ca6198
Pragati Shendage <pragatishantilalshendage3677@gmail.com>
2025-09-08 14:33:07 +0530

    add program

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from Package import math_ops, string_ops
+# from Package import math_ops, string_ops
 
-print("Addition:", math_ops.add(10, 40))
-print("Multiplication:", math_ops.multiply(5, 6))
+# print("Addition:", math_ops.add(10, 40))
+# print("Multiplication:", math_ops.multiply(5, 6))
 
-print("Uppercase:", string_ops.to_upper("pragati"))
-print("Reverse:", string_ops.reverse("Pragati"))
+# print("Uppercase:", string_ops.to_upper("pragati"))
+# print("Reverse:", string_ops.reverse("Pragati"))
+
+# import Package
+# print("Addition:",Package.math_ops.add(10, 40))
+# print("Multiplication:",Package.math_ops.multiply(5, 6))
+
+# print("Uppercase:",Package.string_ops.to_upper("pragati"))
+# print("Reverse:",Package.string_ops.reverse("Pragati"))
+
+from Package.math_ops import add
+print("Addition:",add(10, 40))
